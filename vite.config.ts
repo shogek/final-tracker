332a3533273d9d395a73a495ac0e9aad78ff9cee
shogek <benas.bagdanovas@gmail.com>
2023-03-27 22:03:03 +0300

    Update `faviconURL`

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-let faviconURL = '/favicon.svg'
+let faviconURL = '/final-tracker/favicon.svg'
 
 
 
