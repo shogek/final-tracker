e5009a56e0345c022ca53539f1064f5e52167f03
shogek <benas.bagdanovas@gmail.com>
2023-03-28 21:29:20 +0300

    Use a monospaced font "Martian Mono"

--- src/features/Day/Day.tsx
+++ src/features/Day/Day.tsx
@@ -9,7 +9,9 @@ interface DayProps {
 export default function Day({ }: DayProps): JSX.Element {
    return (
       <article className="day">
-         <h3>2023-03-28  ·  {format(new Date(), 'EEEE')}</h3>
+         <h3 className="day__title">
+            2023-03-28  ·  {format(new Date(), 'EEEE')}
+         </h3>
          <p className="day__line">✓ Fasted 16h</p>
          <p className="day__line">✓ 10k steps</p>
          <p className="day__line">S × 0</p>
